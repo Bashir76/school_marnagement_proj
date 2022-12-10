81022f64f3e74b22eaef6447205dbfc75ffff48b
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-12-10 10:11:09 +0100

    Update footerstud.php

--- footerstud.php
+++ footerstud.php
@@ -5,7 +5,7 @@
           <div class="col-md-3">
             <div class="ftco-footer-widget mb-4">
               <h2><a class="navbar-brand" href="index.php"><i class="flaticon-university"></i> learn portal <br><small>start learning</small></a></h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+              <p>Africa Nigeria,  Kano Nigeria Kano Dala. West Africa. </p>
               <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
                 <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
                 <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
@@ -20,8 +20,8 @@
               <h2 class="ftco-heading-2">Have a Questions?</h2>
               <div class="block-23 mb-3">
                 <ul>
-                  <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                  <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                  <li><span class="icon icon-map-marker"></span><span class="text">2034 Africa Nigeria,  Kano Nigeria Kano Dala</span></li>
+                  <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2348164808800</span></a></li>
                   <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
                 </ul>
               </div>
