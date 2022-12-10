79caae7dff6553c8b5842e772099fa7f5820bcc6
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2022-12-10 09:26:00 +0100

    Update login_admin.php

--- login_admin.php
+++ login_admin.php
@@ -3,11 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="NG School Login In">
+    <meta name="author" content="Bashir Amirkano">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Admin Login | TMS</title>
+    <title>Admin Login | NG School</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/floating-labels/">
 
